14f385182c8cd31db79cef19f0f62623910766e0
Bharathy <abharathy@gmail.com>
2018-09-24 16:49:51 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi I am dinesh from India.</h1>
-    <p>I am a Student.</p>   
-    <p>I love hackathons and FCC.</p>	
+    <h1>Hi I am bharathy from India.</h1>
+    <p>I am a trainer.</p>  
+    <p>We are taking initiative in our college to introduce FCC to students.</p>
+    <p>I love FCC.</p>	
     <p>I Love coding.</p>
-	<p> This is my personal <a href='http://www.dineshmax.ga' target='_blank'>Portfolio</a>. </p>
-	<br></br>
     <b> <i> Thankyou !!! See you all in online... </i> </b>
   </div>
 )
